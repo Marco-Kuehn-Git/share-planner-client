1900f83b454d851f18ea72307036a7aeca280e55
Marco Kühn <marco.kuehn@edu.bib.de>
2021-11-25 11:34:33 +0100

    created new window

--- hellofx/app/src/main/java/client/MainController.java
+++ hellofx/app/src/main/java/client/MainController.java
@@ -1,12 +1,19 @@
 package client;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import res.Event;
 import res.DataController;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class MainController {
     @FXML
@@ -30,6 +37,21 @@ public class MainController {
 
     @FXML
     protected void onAddBtnClick(){
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("create-event.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            scene.getStylesheets().add(Objects.requireNonNull(MainApplication.class.getResource("create-event.css")).toExternalForm());
+            Stage stage = new Stage();
+            stage.setTitle("Termin erstellen");
+            stage.setScene(scene);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+
         DataController dataController = new DataController();
         Event[] eventList = dataController.getAllEvents();
 
