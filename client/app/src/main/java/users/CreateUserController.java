d574849407127e3a7aab1875712370a5731e4049
Alex Rechtin <alexrechtin@gmail.com>
2022-01-10 12:44:27 +0100

    Created create-user.fxml,create-user.css,CreateUserController.java

---
+++ client/app/src/main/java/users/CreateUserController.java
@@ -0,0 +1,12 @@
+package users;
+
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class CreateUserController {
+
+    public TextField textName;
+    public TextField textPassword;
+    public TextField textPasswordSecond;
+    public CheckBox checkBoxIsAdmin;
+}
