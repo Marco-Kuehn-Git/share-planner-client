6e6183bf3818225e10ea1077fd625ab31de0110d
Alex Rechtin <alexrechtin@gmail.com>
2022-01-18 12:45:18 +0100

    Updated create-user, css and the controller

--- client/app/src/main/java/users/CreateUserController.java
+++ client/app/src/main/java/users/CreateUserController.java
@@ -1,10 +1,11 @@
 package users;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -14,27 +15,40 @@ public class CreateUserController {
     public TextField textName;
     public TextField textPassword;
     public TextField textPasswordSecond;
-    public CheckBox checkBoxIsAdmin;
-    public Label labelErrorName;
-    public Label labelErrorPw;
+    public ToggleButton checkButtonIsAdmin;
+    public TextField textLogin;
+    public TextField textForename;
+    public Label labelError;
 
+    @FXML
     protected void createUser(ActionEvent event) {
+        if (textLogin.getText().trim().isEmpty()){
+            labelError.setText("Bitte Login Namen angeben");
+            return;
+        }
+        if (textForename.getText().trim().isEmpty()) {
+            labelError.setText("Bitte Vornamen eingeben!");
+            return;
+        }
         if (textName.getText().trim().isEmpty()) {
-            labelErrorName.setText("Bitte Usernamen eingeben!");
-            labelErrorPw.setText("");
+            labelError.setText("Bitte Nachnamen eingeben!");
             return;
         }
         if (textPassword.getText().trim().isEmpty()) {
-            labelErrorName.setText("");
-            labelErrorPw.setText("Bitte Passwort eingeben!");
+            labelError.setText("Bitte Passwort eingeben!");
             return;
         }
-        if (Objects.equals(textPassword.getText(), textPasswordSecond.getText())){
-            labelErrorName.setText("");
-            labelErrorPw.setText("Passwörter stimmen nicht überein!");
+        if (!Objects.equals(textPassword.getText(), textPasswordSecond.getText())){
+            labelError.setText("Passwörter stimmen nicht überein!");
             return;
         }
 
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    protected void abortBtnClick(ActionEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.close();
     }
