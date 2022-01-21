ea3288a3455092a622b388ea1e85e6662155833f
Marco Kühn <marco.kuehn@edu.bib.de>
2022-01-21 07:58:27 +0100

    Fixed bg-color

--- client/app/src/main/java/main/MainController.java
+++ client/app/src/main/java/main/MainController.java
@@ -9,6 +9,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -30,6 +31,8 @@ public class MainController {
     public VBox leftNav;
     @FXML
     public GridPane mainGridPane;
+    @FXML
+    public HBox buttonBox;
     @FXML
     private GridPane calendarGrid;
 
@@ -59,7 +62,7 @@ public class MainController {
         Group svgAdd = new Group(
                 SvgBtnCreator.createPath("M0 0h24v24H0z", "transparent", "transparent"),
                 SvgBtnCreator.createPath("M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z",
-                        "white", "lightgray")
+                        "white", "gray")
         );
         Button addBtn = SvgBtnCreator.createBtn(svgAdd, 40);
         addBtn.setOnAction(e -> onAddBtnClick());
@@ -69,7 +72,7 @@ public class MainController {
         Group svgSettings = new Group(
                 SvgBtnCreator.createPath("M0 0h24v24H0V0z", "transparent", "transparent"),
                 SvgBtnCreator.createPath("M19.14,12.94c0.04-0.3,0.06-0.61,0.06-0.94c0-0.32-0.02-0.64-0.07-0.94l2.03-1.58c0.18-0.14,0.23-0.41,0.12-0.61 l-1.92-3.32c-0.12-0.22-0.37-0.29-0.59-0.22l-2.39,0.96c-0.5-0.38-1.03-0.7-1.62-0.94L14.4,2.81c-0.04-0.24-0.24-0.41-0.48-0.41 h-3.84c-0.24,0-0.43,0.17-0.47,0.41L9.25,5.35C8.66,5.59,8.12,5.92,7.63,6.29L5.24,5.33c-0.22-0.08-0.47,0-0.59,0.22L2.74,8.87 C2.62,9.08,2.66,9.34,2.86,9.48l2.03,1.58C4.84,11.36,4.8,11.69,4.8,12s0.02,0.64,0.07,0.94l-2.03,1.58 c-0.18,0.14-0.23,0.41-0.12,0.61l1.92,3.32c0.12,0.22,0.37,0.29,0.59,0.22l2.39-0.96c0.5,0.38,1.03,0.7,1.62,0.94l0.36,2.54 c0.05,0.24,0.24,0.41,0.48,0.41h3.84c0.24,0,0.44-0.17,0.47-0.41l0.36-2.54c0.59-0.24,1.13-0.56,1.62-0.94l2.39,0.96 c0.22,0.08,0.47,0,0.59-0.22l1.92-3.32c0.12-0.22,0.07-0.47-0.12-0.61L19.14,12.94z M12,15.6c-1.98,0-3.6-1.62-3.6-3.6 s1.62-3.6,3.6-3.6s3.6,1.62,3.6,3.6S13.98,15.6,12,15.6z",
-                        "white", "lightgray")
+                        "white", "gray")
         );
         Button settingsBtn = SvgBtnCreator.createBtn(svgSettings, 40);
         settingsBtn.setOnAction(e -> onSettingBtnClick());
@@ -79,7 +82,7 @@ public class MainController {
         Group svgLogout = new Group(
                 SvgBtnCreator.createPath("M0 0h24v24H0z", "transparent", "transparent"),
                 SvgBtnCreator.createPath("M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z",
-                        "white", "lightgray")
+                        "white", "gray")
         );
         Button logoutBtn = SvgBtnCreator.createBtn(svgLogout, 40);
         logoutBtn.setOnAction(e -> onLogoutBtnClick());
@@ -95,7 +98,7 @@ public class MainController {
         backBtn.setOnAction(e -> onBackClick());
         backBtn.getStyleClass().add("navBtn");
         GridPane.setColumnIndex(backBtn, 1);
-        mainGridPane.getChildren().add(backBtn);
+        buttonBox.getChildren().add(backBtn);
 
         Group svgToday = new Group(
                 SvgBtnCreator.createPath("M0 0h24v24H0z", "transparent", "transparent"),
@@ -106,7 +109,7 @@ public class MainController {
         todayBtn.setOnAction(e -> onTodayClick());
         todayBtn.getStyleClass().add("navBtn");
         GridPane.setColumnIndex(todayBtn, 2);
-        mainGridPane.getChildren().add(todayBtn);
+        buttonBox.getChildren().add(todayBtn);
 
         Group svgNext = new Group(
                 SvgBtnCreator.createPath("M0 0h24v24H0z", "transparent", "transparent"),
@@ -117,7 +120,7 @@ public class MainController {
         nextBtn.setOnAction(e -> onNextClick());
         nextBtn.getStyleClass().add("navBtn");
         GridPane.setColumnIndex(nextBtn, 3);
-        mainGridPane.getChildren().add(nextBtn);
+        buttonBox.getChildren().add(nextBtn);
     }
 
     private void updateEvents() {
@@ -167,7 +170,6 @@ public class MainController {
             stage.setScene(scene);
             stage.initModality(Modality.APPLICATION_MODAL);
             stage.setResizable(false);
-            //stage.initStyle(StageStyle.UNDECORATED);
             stage.showAndWait();
         } catch (IOException e) {
             e.printStackTrace();
@@ -221,7 +223,7 @@ public class MainController {
         Group svgDel = new Group(
                 SvgBtnCreator.createPath("M0 0h24v24H0z", "transparent", "transparent"),
                 SvgBtnCreator.createPath("M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z",
-                        "white", "lightgray")
+                        "white", "gray")
         );
         Button deleteBtn = SvgBtnCreator.createBtn(svgDel, 24);
 
@@ -235,7 +237,7 @@ public class MainController {
         Group svgEdit = new Group(
                 SvgBtnCreator.createPath("M0 0h24v24H0z", "transparent", "transparent"),
                 SvgBtnCreator.createPath("M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z",
-                        "white", "lightgray")
+                        "white", "gray")
         );
         Button editBtn = SvgBtnCreator.createBtn(svgEdit, 24);
         editBtn.getStyleClass().add("editEventBtn");
